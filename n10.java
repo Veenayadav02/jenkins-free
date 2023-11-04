3a8d6ef8c9bc95abecb7febdd5ef5f8a7793d6fe
nisarr <nisarr@gmail.com>
2023-11-04 12:06:27 +0000

    eee

---
+++ n10.java
@@ -0,0 +1,7 @@
+class n10
+{
+	public static void main(String[] args) {
+
+	System.out.println("hi");
+}
+}
---
+++ n11.java
@@ -0,0 +1,7 @@
+class n11
+{
+        public static void main(String[] args) {
+
+        System.out.println("hi");
+}
+}
